e88e9816918535a77866d5b8ef5b0d4cfab3feff
MatthewTan420 <s10239950@connect.np.edu.sg>
2024-01-30 14:35:41 +0800

    touch up commit

--- Assets/Scripts/Trash.cs
+++ Assets/Scripts/Trash.cs
@@ -7,6 +7,7 @@ public class Trash : MonoBehaviour
 {
     public TextMeshPro textMeshPro;
     public int num = 0;
+    public int subnum = 0;
 
     public GameObject text;
 
@@ -16,12 +17,30 @@ public class Trash : MonoBehaviour
         {
             num += 1;
             textMeshPro.text = num.ToString();
-            Destroy(other);
+            Destroy(other.gameObject);
         }
 
         if (other.gameObject.tag == "Education" && text != null)
         {
             text.SetActive(true);
+        }
+
+        if (other.gameObject.tag == "Fish" && gameObject.tag == "Bucket")
+        {
+            num += 1;
+            textMeshPro.text = num.ToString();
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "Trash" && gameObject.tag == "Bucket")
+        {
+            subnum += 1;
+            if (subnum == 5)
+            {
+                num += 1;
+                textMeshPro.text = num.ToString();
+                subnum = 0;
+            }
+            Destroy(other.gameObject);
         }
     }
 }
